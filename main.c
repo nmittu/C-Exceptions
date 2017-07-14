3e4166ef17994404b18561e5f8d444545cbbeb5b
Nick Mittu <nick@mittudev.com>
2017-07-14 11:44:30 -0400

    fix main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "exceptions.h"
 
 void bad_func() {
-	throw(1, "bad_func: Exception Throws!!");
+	throw(1, "bad_func: Exception Thrown!!");
 }
 
 int main() {
@@ -15,7 +15,15 @@ int main() {
 		if ((e = try(setjmp(jmp))) == NULL) {
 			exception* e2;
 			if ((e2 = try(setjmp(jmp))) == NULL){
+				exception* e3;
+				if ((e3 = try(setjmp(jmp))) == NULL){
+					printf("%s\n", "No exception here");
+					end_try();
+				}else{
+					throw(e3->code, e3->message);
+				}
 				bad_func();
+				end_try();
 			}else{
 				throw(e2->code, e2->message);
 			}
